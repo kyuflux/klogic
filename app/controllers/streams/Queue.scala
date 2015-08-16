987b77c8952cfe55c1055bd89a9647b54dd181a2
javier salcedo <javiersalcedo75@gmail.com>
2015-08-15 22:24:56 -0300

    spec2 testing redis and streams

--- app/controllers/streams/Queue.scala
+++ app/controllers/streams/Queue.scala
@@ -1,19 +1,18 @@
 package controllers.streams
 
 import play.api._
-import play.api.libs.json._
+import libs.json._
 import scredis._
 import akka.actor._
-import play.api.libs.concurrent.Akka
-import play.api.Play.current
+import libs.concurrent.Akka
+import Play.current
 import scala.util.{ Success, Failure }
 import scala.concurrent._
 import ExecutionContext.Implicits.global
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Source, Flow, Sink}
+import akka.stream.scaladsl.{Source, Flow, Sink, FlowGraph}
 import akka.stream.OverflowStrategy.dropHead
-import play.api.libs.ws.WS
-import akka.stream.ActorMaterializer
+import libs.ws.WS
 import Queue._
 
 class Queue(redisHost:String, klHost:String){
@@ -25,29 +24,30 @@ implicit val system = Akka.system
 	val inEnd = Sink.foreach[Enqueue](em => client.rPush[Customer](em.channel,em.data) )
 	
 	val in = Flow[Enqueue].to(inEnd).runWith(inSrc)
-	val outSrc = Source.actorRef[Dequeue](999,dropHead)
+	
 	import Queue.CustomerReader._
-	val lpop = Flow[Dequeue].mapAsync(10){ dq =>
-			Future{
-				client.lPop[String](s"${dq.org}:${dq.branch}:${dq.queue}") map { cm =>
-					Some((s"$klHost/${dq.org}/${dq.branch}",cm))  
-				}
+	val pop = Flow[Dequeue].mapAsync(10){ dq =>
+			client.lPop[Customer](s"${dq.org}:${dq.branch}:${dq.queue}") map { cm =>
+				Some((s"$klHost/${dq.org}/${dq.branch}",cm.get))  
 			}
-		}
-	val filter = Flow[Some[(String,Customer)]].filter( o => !o.isEmpty   )
-	val outEnd = Sink.foreach[Some[(String,Customer)]](o => WS.url(o._1).post(o._2))
-	val out = lpop.join(filter).to(outEnd).runWith(outSrc)
+	}
+	val sourceOut:Source[Some[(String,Customer)],ActorRef] = 
+			Source.actorRef[Dequeue](999,dropHead).via(pop)
+
+	val outEnd = Sink.foreach[Some[(String,Customer)]](
+		o => WS.url(o.get._1).post(Json.toJson(o.get._2)))
+
+	val out = outEnd.runWith(sourceOut)
 
 }
 
 import scredis.serialization._
 object Queue{
 	def apply(redisHost:String, klHost:String):Queue = new Queue(redisHost,klHost)
-	implicit val cusFmt = Json.format[Customer]
-	
+	implicit val cusFmt:Format[Customer] = Json.format[Customer]
 	implicit object CustomerWriter extends Writer[Customer]{
 	override def writeImpl(c:Customer):Array[Byte] = {
-	   	Json.toJson(c).toString.getBytes
+	   	Json.stringify(Json.toJson(c)).getBytes
 	  }
 	}
 	implicit object CustomerReader extends Reader[Customer]{
--- test/QueueSpec.scala
+++ test/QueueSpec.scala
@@ -6,7 +6,17 @@ import play.api.mvc._
 import play.api.test.Helpers._
 import play.api.libs.json._
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent._
 import controllers.streams._
+import akka.stream.scaladsl.{Source, Flow, Sink, FlowGraph}
+import Queue._
+import akka.testkit._
+import akka.stream._
+import scala.concurrent.duration._
+import akka.stream.testkit.scaladsl._
+import akka.stream.testkit.TestSubscriber._
+import akka.pattern.pipe
+import java.lang.Thread
 /**
  * Add your spec here.
  * You can mock out a whole application including requests, plugins etc.
@@ -17,11 +27,27 @@ class QueueSpec extends Specification {
 
   "Queue" should {
 
-    "Enqueue and dequeue customer" in new WithApplication{
-    val q = Queue("10.0.0.99","") 
-	q.in ! Queue.Customer("Javier","Salcedo","17547608")
-
-	"11" must haveSize(2) 
-    }
+    "works without streams, just redis client" in new WithApplication{
+	 val q = Queue("10.0.0.99","") 
+    
+	q.client.rPush[String]("google:california:oldies","javier")
+	val future:Future[Option[String]] = q.client.lPop[String]("google:california:oldies")
+     	val result:Option[String] = Await.result(future, 100.millis)
+       	 result.getOrElse("no") must contain("javier") 
+	}
+    
+    "Enqueue and dequeue customer from google:california:oldies queue" in new WithApplication{
+	 val q = Queue("10.0.0.99","") 
+	val javier = Customer("Javier","Salcedo","17547608")
+	q.in ! Enqueue("google:california:oldies",javier)
+	Thread.sleep(300)
+	implicit val system = q.system
+	import q.materializer	
+	val probe = TestProbe()
+	val source:Source[Some[(String, Customer)],Unit] = Source.single[Dequeue](
+		Dequeue("google","california","oldies")).via(q.pop)
+	source.runWith(Sink.head).pipeTo(probe.ref)
+	probe.expectMsg(400.millis, Some(("/google/california",javier)))
+ }
   }
 }
